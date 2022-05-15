5607ce50e56afdccb5c07ab4797d8d0da7f15916
Joshua Church <joshua.q.church@erdc.dren.mil>
2022-05-15 17:28:40 -0500

    Updated setup.bat path

--- setup.bat
+++ setup.bat
@@ -11,7 +11,7 @@ echo "================================================="
 echo. 
 
 powershell -Command "$ProgressPreference = 'silentlyContinue';"^
- "Invoke-WebRequest http://cran.r-project.org/bin/windows/base/old/4.1.1/R-4.1.1-win.exe -Outfile C:\Users\%username%\Downloads\R-4.1.1-win.exe;"^
+ "Invoke-WebRequest http://cran.r-project.org/bin/windows/base/old/4.1.1/R-4.1.1-win.exe -Outfile $env:USERPROFILE\Downloads\R-4.1.1-win.exe;"^
  "$ProgressPreference = 'Continue';" 
 
 echo. 
@@ -20,7 +20,7 @@ echo.
 
 echo "Step 2: Installing R."
 echo "======================"
-powershell -Command "Start-Process C:\Users\%username%\Downloads\R-4.1.1-win.exe -NoNewWindow -Wait;"
+powershell -Command "Start-Process $env:USERPROFILE\Downloads\R-4.1.1-win.exe -NoNewWindow -Wait;"
 echo .
 echo "R installed!"
 echo. 
@@ -29,16 +29,16 @@ echo "Step 3: Downloading Source Code..."
 echo "=================================="
 
 :: Remove existing version of the Parcel Manifest code first. 
-powershell -Command "Remove-Item -Path C:\Users\%username%\Documents\ParcelManifest-R -Force -Recurse;"
+powershell -Command "Remove-Item -Path $env:USERPROFILE\Documents\ParcelManifest-R -Force -Recurse;"
 
 :: Install most up-to-date copy of the code. 
 powershell -Command "$ProgressPreference = 'silentlyContinue';"^
-    "Invoke-WebRequest https://github.com/JoshuaQChurch/ParcelManifest-R/archive/refs/heads/main.zip -Outfile C:\Users\%username%\Documents\ParcelManifest.zip;"^
+    "Invoke-WebRequest https://github.com/JoshuaQChurch/ParcelManifest-R/archive/refs/heads/main.zip -Outfile $env:USERPROFILE\Documents\ParcelManifest.zip;"^
     "$ProgressPreference = 'Continue';" 
-powershell -Command "Expand-Archive -Path C:\Users\%username%\Documents\ParcelManifest.zip -DestinationPath C:\Users\%username%\Documents -Force;"
-powershell -Command "Remove-Item -Path C:\Users\%username%\Documents\ParcelManifest.zip -Force;"
-powershell -Command "Remove-Item -Path C:\Users\%username%\Documents\ParcelManifest-R -Force -Recurse;"
-powershell -Command "Rename-Item -Path C:\Users\%username%\Documents\ParcelManifest-R-main -NewName ParcelManifest-R -Force;"
+powershell -Command "Expand-Archive -Path $env:USERPROFILE\Documents\ParcelManifest.zip -DestinationPath $env:USERPROFILE\Documents -Force;"
+powershell -Command "Remove-Item -Path $env:USERPROFILE\Documents\ParcelManifest.zip -Force;"
+powershell -Command "Remove-Item -Path $env:USERPROFILE\Documents\ParcelManifest-R -Force -Recurse;"
+powershell -Command "Rename-Item -Path $env:USERPROFILE\Documents\ParcelManifest-R-main -NewName ParcelManifest-R -Force;"
 
 echo.
 echo "Source code downloaded into 'Documents' folder."
@@ -49,10 +49,10 @@ echo "NOTE: This step takes a while (~20 minutes). Please wait..."
 echo "=========================================================="
 
 :: Check if admin or standard user
-if exist "C:\Users\%username%\Documents\R\R-4.1.1\bin\Rscript.exe" (
-    powershell -Command "& 'C:\Users\%username%\Documents\R\R-4.1.1\bin\Rscript.exe' C:\Users\%username%\Documents\ParcelManifest-R\build\electron_build.R"
+if exist "$env:USERPROFILE\Documents\R\R-4.1.1\bin\Rscript.exe" (
+    powershell -Command "& '$env:USERPROFILE\Documents\R\R-4.1.1\bin\Rscript.exe' $env:USERPROFILE\Documents\ParcelManifest-R\build\electron_build.R"
 ) else (
-    powershell -Command "& 'C:\Program Files\R\R-4.1.1\bin\Rscript.exe' C:\Users\%username%\Documents\ParcelManifest-R\build\electron_build.R"
+    powershell -Command "& 'C:\Program Files\R\R-4.1.1\bin\Rscript.exe' $env:USERPROFILE\Documents\ParcelManifest-R\build\electron_build.R"
 )
 echo.
 echo "Parcel Manifest Installed!"
@@ -61,9 +61,9 @@ echo.
 echo "Step 5: Creating Desktop shortcut..."
 echo "====================================="
 
-powershell -Command "Remove-Item -Path C:\Users\%username%\Desktop\ParcelManifest.lnk -Force;"
-powershell -Command "$SourceFileLocation = 'C:\Users\%username%\Documents\ParcelManifest-R\build\output\ParcelManifest\dist\win-unpacked\ParcelManifest.exe';"^
-    "$ShortcutLocation = 'C:\Users\%username%\Desktop\ParcelManifest.lnk';"^
+powershell -Command "Remove-Item -Path $env:USERPROFILE\Desktop\ParcelManifest.lnk -Force;"
+powershell -Command "$SourceFileLocation = '$env:USERPROFILE\Documents\ParcelManifest-R\build\output\ParcelManifest\dist\win-unpacked\ParcelManifest.exe';"^
+    "$ShortcutLocation = '$env:USERPROFILE\Desktop\ParcelManifest.lnk';"^
     "$WScriptShell = New-Object -ComObject WScript.Shell;"^
     "$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation);"^
     "$Shortcut.TargetPath = $SourceFileLocation;"^
